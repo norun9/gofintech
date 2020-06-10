297f089d7c75da6211564474e6954f646d257568
tomoyaueno29 <u.t012807@gmail.com>
2020-06-10 16:20:39 +0900

    Update logging.go

--- utils/logging.go
+++ utils/logging.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func LoggingSettings(logFile string) {
+	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("fire=logFile err=%s", err.Error())
+	}
+	multilogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multilogFile)
+}
