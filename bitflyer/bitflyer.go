4230c195ae45cd0c13d6391b9e514d6cb8fdd9d1
tomoyaueno29 <u.t012807@gmail.com>
2020-06-10 16:45:15 +0900

    Update bitflyer.go

--- bitflyer/bitflyer.go
+++ bitflyer/bitflyer.go
@@ -1,11 +1,13 @@
 package bitflyer
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 )
@@ -28,8 +30,51 @@ func (api APIClient) header(method, endpoint string, body []byte) map[string]str
 	sign := hex.EncodeToString(mac.Sum(nil))
 	return map[string]string{
 		"ACCESS-KEY": api.key,
-		"ACCESS-TIMESRAMP": timestamp,
+		"ACCESS-TIMESTAMP": timestamp,
 		"ACCESS-SIGN": sign,
 		"Content-Type": "application/json",
 	}
 }
+
+func (api *APIClient) doRequest(method, urlPath string, query map[string]string, data []byte) (body []byte, err error) {
+	baseURL, err := url.Parse(baseURL)
+	if err != nil {
+		return
+	}
+	apiURL, err := url.Parse(urlPath)
+	if err != nil {
+		return
+	}
+	endpoint := baseURL.ResolveReference(apiURL).String()
+	log.Printf("action=doRequest", endpoint)
+
+	req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+	q := req.URL.Query()
+	for key, value := range query {
+		q.Add(key, value)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	for key, value := range api.header(method, req.URL.RequestURI(), data) {
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
