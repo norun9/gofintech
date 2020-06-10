c4769876af1cd0b4b6acf2368c54d3ce4fd24333
tomoyaueno29 <u.t012807@gmail.com>
2020-06-10 17:26:21 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"GoFintech/config"
 	"GoFintech/utils"
 	"fmt"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	fmt.Println(apiClient.GetBalance())
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.Datetime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 }
