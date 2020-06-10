7f8f72719bf814970ea2fc057c678565865ec00f
tomoyaueno29 <u.t012807@gmail.com>
2020-06-10 18:05:37 +0900

    Update config.go

--- config/config.go
+++ config/config.go
@@ -6,24 +6,25 @@ import (
 	"os"
 )
 
-type ConfigList struct{
-	ApiKey string
-	ApiSecret string
-	LogFile string
+type ConfigList struct {
+	ApiKey      string
+	ApiSecret   string
+	LogFile     string
+	ProductCode string
 }
 
 var Config ConfigList
 
-func init(){
+func init() {
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
 		log.Printf("Failed to read file: %v", err)
 		os.Exit(1)
 	}
 	Config = ConfigList{
-		ApiKey: cfg.Section("bitflyer").Key("api_key").String(),
+		ApiKey:    cfg.Section("bitflyer").Key("api_key").String(),
 		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
-		LogFile: cfg.Section("gotrading").Key("log_file").String(),
+		LogFile:   cfg.Section("gotrading").Key("log_file").String(),
 	}
 
 }
