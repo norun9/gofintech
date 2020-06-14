18f1634a92b3de5ad5f62aabe10fda760cc9ca23
tomoyaueno29 <u.t012807@gmail.com>
2020-06-14 15:02:41 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"GoFintech/app/controllers"
-	"GoFintech/app/models"
-	"fmt"
 	"GoFintech/config"
 	"GoFintech/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
