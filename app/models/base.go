3523e142bdffe93a81c95b24f9d5878866fdb0cc
tomoyaueno29 <u.t012807@gmail.com>
2020-06-14 13:46:34 +0900

    Update base.go

--- app/models/base.go
+++ app/models/base.go
@@ -1,11 +1,12 @@
 package models
 
 import (
-	"GoFintech/config"
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"GoFintech/config"
 	"log"
+	"time"
 )
 
 const (
@@ -14,20 +15,35 @@ const (
 
 var DbConnection *sql.DB
 
-func int() {
+func GetCandleTableName(productCode string, duration time.Duration) string {
+	return fmt.Sprintf("%s_%s", productCode, duration)
+}
+
+func init() {
 	var err error
 	DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	cmd := fmt.Sprintf(`
-		CREATE TABLE IF NOT EXISTS %s (
-			time DATETIME PRIMARY KEY NOT NULL,
-			product_code STRING,
-			side STRING,
-			price FLOAT,
-			size FLOAT
-		)`, tableNameSignalEvents)
+        CREATE TABLE IF NOT EXISTS %s (
+            time DATETIME PRIMARY KEY NOT NULL,
+            product_code STRING,
+            side STRING,
+            price FLOAT,
+            size FLOAT)`, tableNameSignalEvents)
 	DbConnection.Exec(cmd)
+
+	for _, duration := range config.Config.Durations {
+		tableName := GetCandleTableName(config.Config.ProductCode, duration)
+		c := fmt.Sprintf(`
+            CREATE TABLE IF NOT EXISTS %s (
+            time DATETIME PRIMARY KEY NOT NULL,
+            open FLOAT,
+            close FLOAT,
+            high FLOAT,
+            low open FLOAT,
+			volume FLOAT)`, tableName)
+		DbConnection.Exec(c)
+	}
 }
