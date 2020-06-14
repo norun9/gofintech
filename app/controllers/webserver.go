cbd2ab0971bd11035f71e004924361cb061f371b
tomoyaueno29 <u.t012807@gmail.com>
2020-06-14 15:29:42 +0900

    Update webserver.go

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"GoFintech/app/models"
 	"GoFintech/config"
 	"fmt"
 	"html/template"
@@ -10,7 +11,12 @@ import (
 var templates = template.Must(template.ParseFiles("app/views/google.html"))
 
 func viewChartHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "google.html", nil)
+	limit := 100
+	duration := "1m"
+	durationTime := config.Config.Durations[duration]
+	df, _ := models.GetAllCandle(config.Config.ProductCode, durationTime, limit)
+
+	err := templates.ExecuteTemplate(w, "google.html", df.Candles)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
