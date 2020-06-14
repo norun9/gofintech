51bc42836c12e7029c5cb588f8c43812ca3127eb
tomoyaueno29 <u.t012807@gmail.com>
2020-06-14 16:33:04 +0900

    Update webserver.go

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,18 +1,19 @@
 package controllers
 
 import (
-	"GoFintech/app/models"
-	"GoFintech/config"
 	"fmt"
+	"GoFintech/app/models"
 	"html/template"
 	"net/http"
+
+	"GoFintech/config"
 )
 
 var templates = template.Must(template.ParseFiles("app/views/google.html"))
 
 func viewChartHandler(w http.ResponseWriter, r *http.Request) {
 	limit := 100
-	duration := "1m"
+	duration := "1s"
 	durationTime := config.Config.Durations[duration]
 	df, _ := models.GetAllCandle(config.Config.ProductCode, durationTime, limit)
 
@@ -26,3 +27,4 @@ func StartWebServer() error {
 	http.HandleFunc("/chart/", viewChartHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
+
