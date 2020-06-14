942e200cadea8ae8302ba040a827d00e18a47ef3
tomoyaueno29 <u.t012807@gmail.com>
2020-06-14 13:56:58 +0900

    Update candle.go

--- app/models/candle.go
+++ app/models/candle.go
@@ -1,3 +1,52 @@
 package models
 
+import (
+	"fmt"
+	"time"
+)
 
+type Candle struct {
+	ProductCode string
+	Duration    time.Duration
+	Time        time.Time
+	Open        float64
+	Close       float64
+	High        float64
+	Low         float64
+	Volume      float64
+}
+
+func NewCandle(productCode string, duration time.Duration, timeDate time.Time, open, close, high, low, volume float64) *Candle {
+	return &Candle{
+		productCode,
+		duration,
+		timeDate,
+		open,
+		close,
+		high,
+		low,
+		volume,
+	}
+}
+
+func (c *Candle) TableName() string {
+	return GetCandleTableName(c.ProductCode, c.Duration)
+}
+
+func (c *Candle) Create() error {
+	cmd := fmt.Sprintf("insert into %s (time, open, close, high, low, volume) values (?, ?, ?, ?, ?, ?)", c.TableName())
+	_, err := DbConnection.Exec(cmd, c.Time.Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func (c *Candle) Save() error {
+	cmd := fmt.Sprintf("update %s set open = ?, hight = ?, low = ?, volume = ?, where time = ?", c.TableName())
+	_, err := DbConnection.Exec(cmd, c.Open, c.Close, c.High, c.Low, c.Volume, c.Time.Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+	return err
+}
