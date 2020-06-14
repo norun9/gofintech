e5fa51ec2a766abaf958566c7690bdc0e90bc4d2
tomoyaueno29 <u.t012807@gmail.com>
2020-06-14 13:46:37 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,33 +1,13 @@
 package main
 
 import (
-	"GoFintech/bitflyer"
+	"GoFintech/app/models"
+	"fmt"
 	"GoFintech/config"
 	"GoFintech/utils"
-	"fmt"
-	//"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-
-	//order := &bitflyer.Order{
-	//	ProductCode: config.Config.ProductCode,
-	//	ChildOrderType: "MARKET",
-	//	Side: "BUY",
-	//	Size: 0.01,
-	//	MinuteToExpires: 1,
-	//	TimeInForce: "GTC",
-	//}
-	//res, _ := apiClient.SendOrder(order)
-	//fmt.Println(res.ChildOrderAcceptanceID)
-
-	i := "JRF20201012-144016-14058"
-	params := map[string]string{
-		"product_code": config.Config.ProductCode,
-		"child_order_acceptance_id": i,
-	}
-	r, _ := apiClient.ListOrder(params)
-	fmt.Println(r)
+	fmt.Println(models.DbConnection)
 }
