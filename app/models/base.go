96d2a8ef5c00c25b07bbb1a003cc18727ae94802
tomoyaueno29 <u.t012807@gmail.com>
2020-06-14 13:32:25 +0900

    Update base.go

--- app/models/base.go
+++ app/models/base.go
@@ -1,1 +1,33 @@
 package models
+
+import (
+	"GoFintech/config"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+const (
+	tableNameSignalEvents = "signal_events"
+)
+
+var DbConnection *sql.DB
+
+func int() {
+	var err error
+	DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmd := fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS %s (
+			time DATETIME PRIMARY KEY NOT NULL,
+			product_code STRING,
+			side STRING,
+			price FLOAT,
+			size FLOAT
+		)`, tableNameSignalEvents)
+	DbConnection.Exec(cmd)
+}
